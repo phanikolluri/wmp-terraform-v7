96065db4d2cdef35828ced7649a3534b83f382a5
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2026-04-20 13:22:12 +0530

    pushing terraform project with ansible

--- modules/component/main.tf
+++ modules/component/main.tf
@@ -57,7 +57,7 @@ resource "null_resource" "main" {
     inline = [
       "sudo dnf install python3.13-pip.noarch -y",
       "sudo pip3.13 install ansible",
-      "ansible-pull -i localhost, -U https://github.com/phanikolluri/wmp-ansible-v4.git main.yml  -e env=${var.env} -e component=${var.component}"
+      "ansible-pull -i localhost, -U https://github.com/phanikolluri/wmp-ansible-v4.git main.yml  -e env=${var.env} -e COMPONENT=${var.component}"
     ]
   }
 }
